f61e43e5f05b8c6580016c3942b76f6f3ac6f56f
davidearbola <davidearbola@gmail.com>
2024-04-24 15:39:23 +0200

    M1 - creo array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log(`funziona js`)
+// MILESTONE 1:
+// Create il vostro array con una serie di elementi.
+
+// -   creo array e inserisco gli elementi che faranno parte dell'array (pane, latte, biscotti ecc...)
+
+// creo array
+const listaSpesa = [
+    // inserisco elementi nell'array
+    'Pane',
+    'Latte',
+    'Uova',
+    'Biscotti',
+    'Pasta',
+    'Nutella'
+]
+
+//eseguo console.log per verificare che tutta funziona
+console.log(listaSpesa);
