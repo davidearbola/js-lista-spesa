2166b0824d705ebe1190ab0724efd3410af2f918
davidearbola <davidearbola@gmail.com>
2024-04-24 15:45:52 +0200

    M1 - creo ciclo for e stampo in pagina ogni elemento dell'array

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 
 // -   creo array e inserisco gli elementi che faranno parte dell'array (pane, latte, biscotti ecc...)
 
+// Iterate usando un ciclo FOR per stampare ogni elemento in CONSOLE.
+
+// -   creo ciclo FOR che mi seleziona ad ogni ciclo un elemento dell'array e lo eseguo con il console.log
+
 // creo array
 const listaSpesa = [
     // inserisco elementi nell'array
@@ -15,4 +19,11 @@ const listaSpesa = [
 ]
 
 //eseguo console.log per verificare che tutta funziona
-console.log(listaSpesa);
+// console.log(listaSpesa);
+
+// creo ciclo FOR
+for (let i = 0; i < listaSpesa.length; i++) {
+    const elemento = listaSpesa[i];
+    //stampo con il console log ogni elemento grazie al ciclo for
+    console.log(`Elemento: ${elemento}, che ha come indice ${i}`)
+}
